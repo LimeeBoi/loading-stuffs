501db13b834fddf829f5a2247a8ffdd058988f05
Ethan Wang Lin <ethanwanglin@Ethans-MacBook-Air.local>
2023-10-22 13:55:05 -0700

    我才在中国，我有冰淇淋

--- src/LoadingThing.jsx
+++ src/LoadingThing.jsx
@@ -3,12 +3,12 @@ export default function LoadingThing({ speed, delay, number }) {
   for (let i = 0; i < number; i++) {
     arr.push(i);
   }  
-  
+  console.log(arr.length);
   return (
     <div className="loading-thing-list">      
       {arr.map((id, i) => 
         <div className="loading-thing" key={id} style={{
-          animation: `move ${1 / speed}s cubic-bezier(1, 0, 0, 1) ${i * delay}s infinite alternate both`
+          animation: `move ${1 / speed}s cubic-bezier(1, 0.5, 0.5, 1) ${i * delay}s infinite alternate both`
         }}/>
       )}
     </div>
